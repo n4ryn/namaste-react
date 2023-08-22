41f4f6ae4f24bb0111fd480487d998fa5294f628
Vinay Kumar <104409366+n4ryn@users.noreply.github.com>
2023-08-23 01:31:47 +0530

    Add css,js file and move js code in seprate file

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id: "heading "}, "Namaste World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
